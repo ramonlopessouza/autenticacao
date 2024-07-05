680b73f40b92794756c5e6e7c6774660eb31c822
Ramon <ramon@compilar.com.br>
2024-07-05 15:36:24 -0300

    adicionado css ao projeto

--- register.php
+++ register.php
@@ -6,8 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="register.css">
 </head>
 <body>
-
+<main class="container">
+    <article class="content">
+        <header class="content-header">
+            <h1 class="title">Cadastra-se</h1>
+        </header>
+        <div>
+            <form>
+                <div>
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" placeholder="Fulano de tal">
+                </div>
+                <div>
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" placeholder="teste@gmail.com">
+                </div>
+                <div class="button">
+                    <button type="button">Cadastra-se</button>
+                </div>
+            </form>
+        </div>
+    </article>
+</main>
 </body>
 </html>
