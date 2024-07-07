a764604e41668a3c78464efe22568dc84c8b60f3
Ramon <ramon@compilar.com.br>
2024-07-07 10:44:24 -0300

    adicionado links no rodape do formulario e pagina home

---
+++ home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Início</title>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<main>
+    <header>
+        <div class="mt-4">
+            <a href="home.php" class="ml-4 uppercase font-bold text-black text-center">Início</a>
+            <a href="register.php" class="ml-4 justify-end uppercase font-bold text-black text-center">Cadastre-se</a>
+        </div>
+    </header>
+</main>
+</body>
+</html>
