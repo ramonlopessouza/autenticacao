1d513e0e88f612cd575c9904ae6ae9b59e855735
Ramon <ramon@compilar.com.br>
2024-07-07 10:43:56 -0300

    adicionado links no rodape do formulario e pagina home

--- register.php
+++ register.php
@@ -12,11 +12,11 @@
 </head>
 <body>
 <main class="fixed left-0 top-0 right-0 bottom-0 flex justify-center bg-gradient-to-r from-sky-500 to-indigo-500">
-    <article class="flex flex-wrap content-center w-3/12">
+    <article class="flex flex-wrap content-center w-96">
         <header class="w-full">
             <h1 class="text-3xl uppercase font-bold text-white text-center">cadastra-se</h1>
         </header>
-        <div class="w-full mt-32">
+        <div class="w-full mt-24">
             <form method="post" action="#" autocomplete="off" class="">
                 <div class="w-full flex p-6 rounded-full bg-slate-100/75">
                     <span class="material-symbols-outlined bases-4 mr-4 text-gray-500">notes</span>
@@ -40,7 +40,7 @@
                     </button>
                 </div>
                 <div class="w-full flex items-center p-6 mt-6 rounded-full bg-slate-100/75">
-                    <span class="material-symbols-outlined bases-4 mr-4 text-gray-500">lock_open</span>
+                    <span class="material-symbols-outlined bases-4 mr-4 text-gray-500">lock</span>
                     <input type="password" id="password_repeat" name="password_repeat" placeholder="Repita a senha"
                            class="w-full bg-transparent font-bold outline-none placeholder:text-gray-500 font-bold outline-none"
                            required>
@@ -49,9 +49,20 @@
                     </button>
                 </div>
                 <button type="submit"
-                        class="w-full p-6 mt-20 rounded-full text-white font-bold bg-transparent border border-gray-300 hover:border-gray-100">
+                        class="w-full p-6 mt-12 rounded-full text-white font-bold bg-transparent border border-gray-300 hover:border-gray-100">
                     Cadastra-se
                 </button>
+
+                <div class="w-full flex justify-between mt-6">
+                    <a href="home.php" class="w-fit flex items-center font-bold text-white">
+                        <span class="material-symbols-outlined text-sm mr-1">arrow_back</span>
+                        <span class="text-sm hover:underline">Voltar para o site</span>
+                    </a>
+                    <a href="home.php" class="w-fit flex items-center font-bold text-white">
+                        <span class="material-symbols-outlined text-sm mr-1">person_check</span>
+                        <span class="text-sm hover:underline">Já tenho conta</span>
+                    </a>
+                </div>
             </form>
         </div>
     </article>
